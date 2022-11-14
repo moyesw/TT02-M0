6c227f01ff308db14c543fd2024573fbd6e435fc
moyesw <116853609+moyesw@users.noreply.github.com>
2022-11-14 00:55:12 -0600

    Update core.v

--- src/core.v
+++ src/core.v
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// M0 top level
+// M0 - 16-bit serial SUBLEQ processor
 //
 // Copyright 2022 William Moyes
 //
@@ -7,12 +7,116 @@
 `default_nettype none
 `timescale 100us/10ps
 
+
+///////////////////////////////////////////////////////////////////////////
+// SPI Controller
+//
+// 16-bit Address + 16-bit Data controller and timing generator
+//
+module SPIController (
+  // System Interfaces
+  input wire clk,
+  input wire rst,
+
+  // SPI Bus Interfaces
+  output reg CS0,
+  output reg CS1,
+  output reg SPICLK,
+  output reg MOSI,
+  input wire MISO,
+
+  // Input Signals
+  input wire Addr15, 		// Sampled on Phase 01
+  input wire Read_notWrite,	// Sampled on Phase 16
+  input wire Addr,		// Sampled on Phase 18[bit0/LSB], 20[bit1], ..., 44[bit13], 46[bit14/MSB], bit 15 not sampled (see Addr15)
+  input wire Data,		// Sampled on Phase 50[bit0/LSB], 52[bit1], ..., 78[bit14], 80[bit15/MSB]
+
+
+  // Timing Output Signals
+  output reg ShiftAddr,		// Asserted when the Address should be shifted
+  output reg ShiftDataRead,     // Asserted when the data register collecting data read from memory should be shifted
+  output reg ShiftDataWrite,    // Asserted when the data regsiter providing data to be written to memory should be shifted
+  output reg PresetCarry,       // Asserted the clock before data motion starts
+  output reg EndOfPhase         //
+);
+
+  // SPI sequencer
+  reg [6:0] SPIphase;
+  always @(posedge clk) begin
+    if (rst)
+      SPIphase <= 0;
+    else if (SPIphase == 83)
+      SPIphase <= 0;
+    else
+      SPIphase <= SPIphase + 1;
+  end
+
+  // SPI bus signal generator
+  always @(posedge clk) begin
+    if (SPIphase <= 1) begin
+      CS0 <= 1;
+      CS1 <= 1;
+      SPICLK <= 0;
+      MOSI <= 0;
+    end else begin
+      CS0 <= CSreg;
+      CS1 <= !CSreg;
+      if (SPIphase <= 81)
+        SPICLK <= SPIphase[0];
+      else
+        SPICLK <= 0;
+
+      if (SPIphase <= 13)
+        MOSI <= 0;
+      else if (SPIphase <= 15)
+        MOSI <= 1;
+      else if (SPIphase <= 17) begin
+        if (SPIphase[0] == 0)
+          MOSI <= Read_notWrite;
+      end else if (SPIphase <= 47) begin
+        if (SPIphase[0] == 0)
+          MOSI <= Addr;		// TODO: Generate the Address Shift timing pulse output
+      end else if (SPIphase <= 49)
+        MOSI <= 0;
+      else begin
+        if (Read_notWrite)
+          MOSI <= 0;
+        else begin
+          if (SPIphase[0] == 0)
+            MOSI <= Data;      	// TODO: Generate the Address Shift timing pulse output
+        end
+      end
+    end
+  end
+
+  // Generate Address Shift Enable Signals
+  always @(posedge clk) begin
+    ShiftAddr <= ((SPIphase >= 18) && (SPIphase <= 48) && (SPIphase[0] == 0));
+    ShiftDataRead <= ((SPIphase >= 51) && (SPIphase <= 81) && (SPIphase[0] == 1) && Read_notWrite);
+    ShiftDataWrite <= ((SPIphase >= 50) && (SPIphase <= 80) && (SPIphase[0] == 0) && !Read_notWrite);
+    PresetCarry <= (SPIphase == 17);
+    EndOfPhase <= (SPIphase == 83);
+  end
+
+  reg CSreg;
+  always @(posedge clk) begin
+    if (SPIphase == 1)
+      CSreg <= Addr15;
+  end
+
+endmodule
+
+
+
+///////////////////////////////////////////////////////////////////////////
+// M0 top level
+//
 module moyes0_top_module (
   input  [7:0] io_in,
   output [7:0] io_out
 );
 
-  // --- Inputs ---
+  // --- ASIC Inputs ---
   wire clk     = io_in[0];      // System clock (~6000 Hz)
   wire rst     = io_in[1];      // Reset line, active high
   wire spi_miso= io_in[2];      // SPI bus, ASIC input, target output
@@ -22,7 +126,7 @@ module moyes0_top_module (
   wire in6     = io_in[6];
   wire in7     = io_in[7];
 
-  // --- Outputs ---
+  // --- ASIC Outputs ---
   wire spi_cs0;
   wire spi_cs1;
   wire spi_clk;
@@ -42,31 +146,60 @@ module moyes0_top_module (
   assign io_out[6] = out6;
   assign io_out[7] = out7;
 
+  // --- Internal Timing Signals ---
+  wire ShiftAddr;
+  wire ShiftDataRead;
+  wire ShiftDataWrite;
+  wire PresetCarry;
+  wire EndOfPhase;
 
-  reg [6:0]  SPIphase;
-  always @(posedge clk) begin
-    if (rst)
-      SPIphase <= 7'h41;
-    else if (SPIphase == 7'h53)
-      SPIphase <= 7'h00;
-    else
-      SPIphase <= SPIphase + 1;
-  end
+  // --- SPI Control Signals
+  wire Addr15;
+  wire Read_notWrite;
+  wire SPIAddr;
+  wire SPIDataIn;
+
+  // --- CPU Registers ---
+  reg [15:0] PC;
+  reg [15:0] TMP;
+  reg [15:0] ADR;
+  reg PCCarry;
+  reg TBorrow;
+  reg TZero;
+  reg LEQ;
 
 
-  wire SPI_Addr = ((SPIphase == 7'h53) || (SPIphase < 7'h1F)) && SPIphase[0];
-  wire SPI_Data = ((SPIphase >= 7'h1F) && (SPIphase <= 7'h3E)) && SPIphase[0];
-  wire SPI_LastBit = (SPIphase == 7'h41);
+  SPIController spi (
+     // System Interfaces
+    .clk(clk),
+    .rst(rst),
 
-  reg ADBit;
-  reg A15;
-  reg Read_notWrite;
+    // SPI Bus Interfaces
+    .CS0(spi_cs0),
+    .CS1(spi_cs1),
+    .SPICLK(spi_clk),
+    .MOSI(spi_mosi),
+    .MISO(spi_miso),
+
+    // Input Signals
+    .Addr15(Addr15),
+    .Read_notWrite(Read_notWrite),
+    .Addr(SPIAddr),
+    .Data(SPIDataIn),
+
+    // Timing Output Signals
+    .ShiftAddr(ShiftAddr),
+    .ShiftDataRead(ShiftDataRead),
+    .ShiftDataWrite(ShiftDataWrite),
+    .PresetCarry(PresetCarry),
+    .EndOfPhase(EndOfPhase)
+  );
 
   reg [2:0]  CPUphase;
   always @(posedge clk) begin
     if (rst)
-      CPUphase <= 3'd5;
-    else if (!SPI_LastBit)
+      CPUphase <= 3'd0;
+    else if (!EndOfPhase)
       CPUphase <= CPUphase;
     else begin
       if (CPUphase == 3'd5)
@@ -76,51 +209,79 @@ module moyes0_top_module (
     end
   end
 
+
   always @(posedge clk) begin
-    // TODO:  This is a hack... replace with real code
-    A15 <= 1'b0;
-    ADBit <= 1'b0;
-    Read_notWrite <= 1'b1;
+    if (rst)  // TODO: Evalute if shifting in 0's is better
+      PC  <= 16'h0000;
   end
 
+  wire PCphase = (CPUphase == 0) || (CPUphase == 2) || (CPUphase == 5);
 
-  reg r_cs0;
-  reg r_cs1;
-  reg r_clk;
-  reg r_mosi;
+  assign Addr15 = PCphase ? PC[15] : ADR[15];
+
+  assign Read_notWrite = (CPUphase != 4);
 
   always @(posedge clk) begin
+    if (PresetCarry)
+      PCCarry <= 1;
 
-    // Drive CS0 and CS1
-    if (!A15) begin
-      r_cs0 <= rst || (SPIphase == 7'h41) || (SPIphase == 7'h42);
-      r_cs1 <= 1;
-    end else begin
-      r_cs1 <= rst || (SPIphase == 7'h41) || (SPIphase == 7'h42);
-      r_cs0 <= 1;
+    if (PCphase && ShiftAddr) begin
+      PCCarry <= PC[0] & PCCarry;
+      PC <= {PC[0] ^ PCCarry, PC[15:1]};
     end
 
-    // Drive SPI_CLK
-    r_clk <= SPIphase[0] && (SPIphase != 7'h41) && (SPIphase != 7'h43);
-
-    // Drive MOSI
-    if (SPIphase < 7'h40)
-      r_mosi <= ADBit;
-    else if (SPIphase < 7'h50)
-      r_mosi <= 1'b0;
-    else if (SPIphase < 7'h52)
-      r_mosi <= 1'b1;
-    else
-      r_mosi <= Read_notWrite;
+    if ((CPUphase == 5) && ShiftDataRead) begin
+      PC <= {LEQ ? spi_miso : PC[0], PC[15:1]};
+    end
   end
 
-  assign spi_cs0 = r_cs0;
-  assign spi_cs1 = r_cs1;
-  assign spi_clk = r_clk;
-  assign spi_mosi = r_mosi;
+  assign SPIAddr = PCphase ? PC[0] : ADR[0];
 
+  assign SPIDataIn = TMP[0];
+
+  wire ReadADR = (CPUphase == 0) || (CPUphase == 2);
+  wire ReadTMP = (CPUphase == 1) || (CPUphase == 3);
+
+  always @(posedge clk) begin
+    if (ReadADR & ShiftDataRead)
+      ADR <= {spi_miso, ADR[15:1]};
+
+    if (!PCphase & ShiftAddr)
+      ADR <= {ADR[0], ADR[15:1]};
+  end
+
+
+  wire sub_b;
+  wire sub_r;
+  assign {sub_b, sub_r} = spi_miso - TMP[0] - TBorrow;
+
+  always @(posedge clk) begin
+    if (PresetCarry) begin
+      TBorrow <= 0;
+      TZero <= 1;
+    end
+
+    if ((CPUphase == 1) & ShiftDataRead)
+      TMP <= {spi_miso, TMP[15:1]};
+
+    if ((CPUphase == 3) & ShiftDataRead) begin
+      TBorrow <= sub_b;
+      TMP <= {sub_r, TMP[15:1]};
+      if (sub_r)
+        TZero <= 0;
+    end
+
+    if (!Read_notWrite & ShiftDataWrite)
+      TMP <= {TMP[0], TMP[15:1]};
+
+  end
+
+  always @(posedge clk) begin
+    if (EndOfPhase & (CPUphase == 3)) begin
+      LEQ <= TZero | TBorrow;
+    end
+  end
 
 
 
 endmodule
-
